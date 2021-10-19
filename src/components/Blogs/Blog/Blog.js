ec73246f3b157a3ca376a8b3cd288bc1fc1a4428
SyedHasibRahman <syedhasib01@gmail.com>
2021-10-19 20:01:51 +0600

    Allhamdulillah! Blog also completed

--- src/components/Blogs/Blog/Blog.js
+++ src/components/Blogs/Blog/Blog.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
-const Blog = () => {
+const Blog = (props) => {
+    const { id, img, name, description } = props.blog;
+    console.log(props.blog);
     return (
         <div>
-            <h2>This is Blog</h2>
+            <div className="col service-card mb-5">
+                <div className="card">
+                    <img src={img} className="card-img-top p-3" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">{name}</h5>
+                        <p className="card-text">{description}</p>
+                    </div>
+
+                </div>
+            </div>
         </div>
     );
 };
